04847b2954be0a72185d66cfabbc111e5c4ca7e6
Christian Henry <christian.henry92@gmail.com>
2019-04-18 22:49:54 -0400

    Remove backend

--- default.nix
+++ default.nix
@@ -3,24 +3,11 @@
 reflex-platform.project ({ pkgs, ... }: {
   packages = {
     common = ./common;
-    backend = ./backend;
     frontend = ./frontend;
   };
 
-  android.frontend = {
-    executableName = "frontend";
-    applicationId = "org.example.frontend";
-    displayName = "Example Android App";
-  };
-
-  ios.frontend = {
-    executableName = "frontend";
-    bundleIdentifier = "org.example.frontend";
-    bundleName = "Example iOS App";
-  };
-
   shells = {
-    ghc = ["common" "backend" "frontend"];
+    ghc = ["common" "frontend"];
     ghcjs = ["common" "frontend"];
   };
 })
