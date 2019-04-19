ba3638c73e209e777604757df28771d311e81c64
Christian Henry <christian.henry92@gmail.com>
2019-04-19 15:39:17 -0400

    Test deploy

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
 { reflex-platform ? import ./reflex-platform {} }:
 
 reflex-platform.project ({ pkgs, ... }: {
+  withHoogle = false;
+  useWarp = true;
+
   packages = {
     common = ./common;
     frontend = ./frontend;
@@ -9,5 +12,15 @@ reflex-platform.project ({ pkgs, ... }: {
   shells = {
     ghc = ["common" "frontend"];
     ghcjs = ["common" "frontend"];
+  };
+
+  shellToolOverrides = ghc: super: {
+    cabal-install = pkgs.haskellPackages.cabal-install;
+    hlint = pkgs.haskellPackages.hlint;
+  };
+
+  overrides = self: super: {
+    # http-media = pkgs.haskell.lib.dontCheck super.http-media;
+    jsaddle-wkwebview = pkgs.haskell.lib.dontCheck super.jsaddle-wkwebview;
   };
 })
